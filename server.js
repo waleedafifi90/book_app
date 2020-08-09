a9399112b5e948576feac7b6d607a6ebca77a999
Waleed A. Afifi <waleedafifich@gmail.com>
2020-08-09 12:42:21 +0300

    Feature 1 finish

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+'use strict';
+require('dotenv').config();
 
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/hello',(req,res)=>{
+  res.render('./pages/index.ejs');
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Listening to Port ${PORT}`);
+});
