6435cf75a2996b5cebd053d45c0db8b6191ce9f8
Waleed A. Afifi <waleedafifich@gmail.com>
2020-08-09 14:37:15 +0300

    add neww file new.ejs and create book constructor

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express()
+const superagent = require('superagent')
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -11,6 +12,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
+app.get('/',(req,res)=>{
+  res.render('./pages/index.ejs');
+});
 
 app.get('/hello',(req,res)=>{
   res.render('./pages/index.ejs');
@@ -19,6 +23,29 @@ app.get('/hello',(req,res)=>{
 app.get('/search/new',(req,res)=>{
   res.render('./pages/searches/new.ejs');
 });
+
+app.post('/searches',(req,res)=>{
+  var searchKey = req.body.bookSearch;
+  var searchType = req.body.searchType;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${searchKey}+in${searchType}`;
+
+  superagent.get(url)
+    .then(bookData =>{
+      let result = bookData.body.items.map(element => {
+        return new Book(element);
+      });
+      res.render('./pages/searches/show',{booksResult: result });
+
+    });
+});
+
+function Book(book) {
+  this.author = book && book.volumeInfo && book.volumeInfo.authors || 'Author Unknown';
+  this.title = book && book.volumeInfo && book.volumeInfo.title || 'Title Missing';
+  this.isbn = book && book.volumeInfo && book.volumeInfo.industryIdentifiers && book.volumeInfo.industryIdentifiers[0] && book.volumeInfo.industryIdentifiers[0].type + book.volumeInfo.industryIdentifiers[0].identifier || 'ISBN Missing';
+  this.image_url = book && book.volumeInfo && book.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
+  this.description = book && book.volumeInfo && book.volumeInfo.description || 'Description Missing';
+}
 
 app.listen(PORT, ()=>{
   console.log(`Listening to Port ${PORT}`);
